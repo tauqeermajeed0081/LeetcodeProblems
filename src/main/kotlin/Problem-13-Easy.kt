e10d35594e691700822d23ba7b5febf39ebebfe5
tauqeermajeedsportsgamex <tauqeermajeed008@gmail.com>
2025-10-16 15:48:41 +0500

    Initial commit

---
+++ src/main/kotlin/Problem-13-Easy.kt
@@ -0,0 +1,28 @@
+package org.example
+
+fun romanToInt(romanString: String): Int {
+    var convertedInt = 0
+    for (index in romanString.indices) {
+        val currentCharInt = matchRomanWithInt(romanString[index])
+        val nextCharInt = if (index + 1 < romanString.length) matchRomanWithInt(romanString[index + 1]) else 0
+        if (currentCharInt >= nextCharInt) {
+            convertedInt += currentCharInt
+        } else
+            convertedInt -= currentCharInt
+    }
+
+    return convertedInt
+}
+
+fun matchRomanWithInt(romanChar: Char): Int {
+    when (romanChar) {
+        'I' -> return 1
+        'V' -> return 5
+        'X' -> return 10
+        'L' -> return 50
+        'C' -> return 100
+        'D' -> return 500
+        'M' -> return 1000
+    }
+    return 0
+}
